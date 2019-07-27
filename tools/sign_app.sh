1fb8d5eabd27545859e782add0f33b31a2a08771
thomas-broussard <thomas.broussard91@gmail.com>
2019-07-27 23:23:07 +0200

    signature app tool added

---
+++ tools/sign_app.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+
+# 1 - générer la release build
+echo "==========================="
+echo "Step 1 - Release Build"
+echo "==========================="
+cd ..
+ionic cordova build --release android
+
+# 2 - Signer l'APK
+echo "==========================="
+echo "Step 2 - APK Signature"
+echo "==========================="
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore tools/HackberryApp.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk HackberryApp 
+
+# 3 - Utiliser zipalign pour optimiser l'APK
+echo "==========================="
+echo "Step 3 - Generate Output"
+echo "==========================="
+./tools/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk outputs/HackberryApp.apk
+
+
+echo "HackberryApp.apk is now available in HackberryApp/outputs/ folder"
