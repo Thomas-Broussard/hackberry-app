dfa9bae8b3ce52a60ee43412692510771f5feace
thomas-broussard <thomas.broussard91@gmail.com>
2019-07-28 13:12:30 +0200

    motors and sensors calibration added to tab2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   { path: 'my-hand-infos', loadChildren: './my-hand/infos/infos.module#InfosPageModule' },
   { path: 'my-hand-sensor', loadChildren: './my-hand/sensor/sensor.module#SensorPageModule' },
   { path: 'my-hand-motors', loadChildren: './my-hand/motors/motors.module#MotorsPageModule' },
+  { path: 'my-hand-motors-calib', loadChildren: './my-hand/motors-calib/motors-calib.module#MotorsCalibPageModule' },
+  { path: 'my-hand-sensor-calib', loadChildren: './my-hand/sensor-calib/sensor-calib.module#SensorCalibPageModule' },
+
+
 ];
 @NgModule({
   imports: [
