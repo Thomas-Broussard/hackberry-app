9e7c7cd72a8b34280734a32fa0d9e55227f4b1af
thomas-broussard <thomas.broussard91@gmail.com>
2019-07-28 13:12:51 +0200

    build APK (test only)

--- tools/sign_app.sh
+++ tools/sign_app.sh
@@ -12,13 +12,13 @@ ionic cordova build --release android
 echo "==========================="
 echo "Step 2 - APK Signature"
 echo "==========================="
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore tools/HackberryApp.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk HackberryApp 
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore tools/HackberryApp.keystore platforms/android/app/build/outputs/apk/release/app-release.apk HackberryApp 
 
 # 3 - Utiliser zipalign pour optimiser l'APK
 echo "==========================="
 echo "Step 3 - Generate Output"
 echo "==========================="
-./tools/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk outputs/HackberryApp.apk
+./tools/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release.apk outputs/HackberryApp.apk
 
 
 echo "HackberryApp.apk is now available in HackberryApp/outputs/ folder"
