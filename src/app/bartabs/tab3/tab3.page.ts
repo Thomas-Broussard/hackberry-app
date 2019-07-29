89e394a33a0eb1eaa46a483f8e327a2fe65e463f
thomas-broussard <thomas.broussard91@gmail.com>
2019-07-30 01:05:31 +0200

    new method to open PDF doc in tab3

--- src/app/bartabs/tab3/tab3.page.ts
+++ src/app/bartabs/tab3/tab3.page.ts
@@ -10,4 +10,21 @@ export class Tab3Page {
 
   constructor(private pdf : PdfService) {}
 
+  downloadRequired : boolean = false
+
+  // list all documents here
+  assemblyGuide : PDFDocument = { name :"assembly-guide", url:"https://github.com/Thomas-Broussard/hackberry-app/raw/master/src/assets/pdf/assembly-guide.pdf"};
+
+  /**
+   * open the doc if present in memory or download it if not
+   */
+  openDoc(doc)
+  {
+    this.pdf.openOrDownload(doc.name,doc.url); 
+  }
+}
+
+class PDFDocument{
+  public url: string;
+  public name: string;
 }
