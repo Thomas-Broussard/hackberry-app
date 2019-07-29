1b8cbcbc433d745d3bf765be3b8e49ae2499e457
thomas-broussard <thomas.broussard91@gmail.com>
2019-07-29 23:34:56 +0200

    script with required plugins for pdf viewer

---
+++ tools/install_pdf_components.sh
@@ -0,0 +1,12 @@
+cd ..
+# Ionic Native Packages
+npm i @ionic-native/file@beta
+npm i @ionic-native/file-opener@beta
+npm i @ionic-native/file-transfer@beta
+npm i @ionic-native/document-viewer@beta
+
+# Cordova Plugins
+ionic cordova plugin add cordova-plugin-file
+ionic cordova plugin add cordova-plugin-file-opener2
+ionic cordova plugin add cordova-plugin-file-transfer
+ionic cordova plugin add cordova-plugin-document-viewer
