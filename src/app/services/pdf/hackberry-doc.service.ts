0e4ffd9e91a901983c6fce1fd05f06016a32152f
thomas-broussard <thomas.broussard91@gmail.com>
2019-07-31 23:54:04 +0200

    improvement of hackberryDoc service - Docs are now downloaded as language pack - popup are displayed during download

--- src/app/services/pdf/hackberry-doc.service.ts
+++ src/app/services/pdf/hackberry-doc.service.ts
@@ -1,9 +1,12 @@
+import { GeneralService } from 'src/app/services/general.service';
+import { AlertController } from '@ionic/angular';
 import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { File } from '@ionic-native/File/ngx';
 import { Injectable } from '@angular/core';
 import { PdfService } from './pdf.service';
 import { strict } from 'assert';
 import { stringify } from '@angular/compiler/src/util';
+import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +17,9 @@ export class HackberryDocService {
   constructor(
     private pdf : PdfService,
     private file: File, 
-    private ft: FileTransfer 
+    private ft: FileTransfer ,
+    private alertController: AlertController,
+    private gen : GeneralService,
     ) { }
 
   private mainURL: string = "https://github.com/Thomas-Broussard/hackberry-docs/raw/master/";
@@ -31,26 +36,48 @@ export class HackberryDocService {
     return this.mainURL + countryCode + '/' + name + ".pdf";
   }
 
+  /**
+   * open a document from the phone memory
+   * @param name name of the document to download
+   * @param countryCode language wanted (fr, en , jp)
+   */
+  open(name: string, countryCode: string)
+  {
+    var languageName = name + '_' + countryCode;
+    this.pdf.open(languageName);
+  }
+
+  /**
+   * open (or download if needed) a document from the phone memory (or Hackberry Github)
+   * @param name name of the document to download
+   * @param countryCode language wanted (fr, en , jp)
+   * @deprecated
+   */
   openOrDownload(name: string, countryCode: string)
   {
     var url = this.getFileURL(name,countryCode);
     var languageName = name + '_' + countryCode;
-    this.pdf.openOrDownload(languageName,url);
+    return this.pdf.openOrDownload(languageName,url);
   }
 
+  
+  /**
+   * download a document from Hackberry Github
+   * @param name name of the document to download
+   * @param countryCode language wanted (fr, en , jp)
+   */
   download(name: string, countryCode: string)
   {
     var url = this.getFileURL(name,countryCode);
     var languageName = name + '_' + countryCode;
-    this.pdf.download(languageName,url);
+    return this.pdf.download(languageName,url);
   }
 
-
-  /**
+    /**
      * download all pdf files from the repository and save it in dataDirectory/pdf/
      * @param url url to download the file
      */
-    downloadAll(countryCode : string)
+    launchDownloadLanguagePack(countryCode : string)
     {
       var url = this.rawURL + 'list.json';
       let me = this;
@@ -65,85 +92,207 @@ export class HackberryDocService {
           (entry) => 
             {
               console.log('list.json downloaded',entry);
-              me.downloadFilesFromList(countryCode);
+              return me.downloadFilesFromList(countryCode)
+              .then(
+                result =>{ 
+                  console.log("downloadFilesFromList Promise = " + result);
+                  return result;
+                }
+              );
             }
         )
         .catch(
           (err) =>{
             console.log('error : list.json not downloaded',err);
+            return false;
           }
         )
       )
       .catch(
-        // Directory does not exists, create a new one, then download and open the file
+        // Directory does not exists, create a new one, then relaunch the function
         err => 		
           me.file.createDir(me.pdf.docPath, me.pdf.docDir, false)
           .then(
             response => {
               console.log('Directory created',response);
-              fileTransfer.download(url,me.pdf.docFullPath + 'list.json').then((entry) => 
-              {
-                  console.log('list.json downloaded',entry);
-                  me.downloadFilesFromList(countryCode);
-              })
-              .catch((err) =>{
-                  console.log('error : list.json not downloaded',err);
-              });
+                return me.launchDownloadLanguagePack(countryCode);
             }
           )
           .catch(
             err => {
               console.log('error : Could not create directory',err);
+              return false;
             }
           ) 
       );
     }
 
+    /**
+     * download all files listed in list.json
+     * @param countryCode language wanted (fr, en , jp)
+     */
     downloadFilesFromList(countryCode: string)
     {
+      this.gen.popupTemp("Downloading [" + countryCode + "] Documentation...", 600000);
       let me = this;
       console.log(me.pdf.docFullPath + "/" +  'list.json');
-      return this.file.readAsText(me.pdf.docFullPath, 'list.json')
-      .then(
-        content => 
-        {
 
-          var fileList = JSON.parse(content);
-          fileList.forEach(element => {
-            me.download(element.filename,countryCode);
-          });
-          
-          return fileList;
-        })
-        .catch(
+      // Read the file list.json
+      return this.getFilesFromList()
+      .then(
+        fileList => 
+        {
+          console.log("getFilesFromList Promise  = " + fileList);
+          return me.downloadList(fileList,countryCode)
+          .then(
+            result =>{
+              console.log("downloadList Promise = " + result);
+              return result;
+            }
+          );    
+        }
+      )
+      .catch(
           err =>
           {
-            console.log('error : list does not exist',err);
-            return [];
+            console.log('errorDL : list does not exist',err);
+            return false;
           }
         );
     }
 
+    /**
+     * get all the documents from the list.json as an array of objects
+     */
     getFilesFromList()
     {
       let me = this;
       return this.file.readAsText(me.pdf.docFullPath, 'list.json').then(
         content => 
         {
-          /* file list here */
           let fileList = JSON.parse(content); 
-          console.log("files : " + content); 
-          me.docList = fileList;
-          
+          //console.log("files : " + content); 
+          me.docList = JSON.parse(content);
           return fileList;
         })
         .catch(
           err =>
           {
-            console.log('error : list does not exist',err);
+            console.log('errorGET : list does not exist',err);
             return [];
           }
         );
+    }
+
+
+    /**
+     * download multiple files fromp a list.json
+     * @param list list of files (see list.json file)
+     * @param countryCode language wanted (fr, en , jp)
+     */
+    downloadList(list: any[], countryCode: string): Promise<Boolean>
+    {
+      if (list.length > 0 )
+      {
+        let me = this;
+        var file = list.shift();
+  
+        return this.download(file.filename,countryCode).then(
+          _=>{
+            return me.downloadList(list,countryCode);
+          }
+        )
+      }
+      // download finished
+      else
+      {
+        return Promise.resolve(true);
+      }
+    }
+
+
+    /**
+     * Open a pop up windows where user can confirm if he want (or not) to download the documentation
+     * @param countryCode language wanted (fr, en , jp)
+     */
+    async downloadLanguagePack(countryCode: string, enablePopUp: boolean = false)
+    {
+      let me = this;
+      let message = "Download the Documentation Pack [" + countryCode + "]? (internet required)";
+      const alert = await this.alertController.create({
+        header: message,
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          }, {
+            text: 'OK',
+            handler: () => {
+              if (enablePopUp == true)
+              {
+                me.gen.popupTemp("Downloading Documentation Pack [" + countryCode + "]...", 600000);
+              }
+              me.launchDownloadLanguagePack(countryCode).then(
+                result =>
+                {
+                  if(enablePopUp == true)
+                  {
+                    me.gen.dismiss();
+                    me.gen.popupTemp("Documentation Pack [" + countryCode + "] successfully downloaded !", 2000);
+                  }
+                }
+              ); 
+            }
+          }
+        ]
+      });
+      await alert.present();
+    }
+
+    /**
+     * Open a pop up windows where user can confirm if he want (or not) 
+     * to download the documentation for each language
+     */
+    async downloadAllPack()
+    {
+      let me = this;
+      let message = "Download the Documentation Pack for each language ? (internet required)";
+      const alert = await this.alertController.create({
+        header: message,
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          }, {
+            text: 'OK',
+            handler: () => {
+              
+              me.launchDownloadLanguagePack('fr').then(
+                result =>{
+                  if (result == true){
+                    me.launchDownloadLanguagePack('en').then(
+                      result =>{
+                        if (result == true){
+                          me.launchDownloadLanguagePack('jp').then(
+                            result =>{
+                              if (result == true){
+                                me.gen.dismiss();
+                                me.gen.toastTemp("Documentations successfully downloaded !",2000);
+                              }
+                            }
+                          )
+                        }
+                      }
+                    )
+                  }
+                }
+              )
+
+            }
+          }
+        ]
+      });
+      await alert.present();
     }
 }
 
